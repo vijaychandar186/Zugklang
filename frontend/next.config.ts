f6cf29250679b886ba96bd86f4d077f98419daeb
forcohere2025 <forcohere2025@hotmail.com>
2026-02-19 05:35:18 +0000

    Updated

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,12 +3,12 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
+  turbopack: {
+    root: __dirname
+  },
   experimental: {
     serverActions: {
-      allowedOrigins: [
-        'localhost:3000',
-        'https://bug-free-orbit-5g96x4w4rjjvcrrg-3000.app.github.dev'
-      ]
+      allowedOrigins: ['localhost:3000']
     }
   },
   async headers() {
